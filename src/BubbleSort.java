41941dc0d5372d6e7ca3925106e1898037101cc6
Atif Iqbal Butt <96383720+AtifButt123@users.noreply.github.com>
2023-06-27 08:42:21 +0500

    Update BubbleSort.java

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.sql.SQLOutput;
+
+public class BubbleSort implements ISortingStrategy{
+
+    private static void swap(int[]nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    @Override
+    public int[] sort(int[]nums) {
+        System.out.println("This is bubble sort");
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++){
+            boolean isSwapped = false;
+            for(int j = 0; j < n-1-i; j++){
+                if(nums[j+1] < nums[j]){
+                    swap(nums, j, j+1);
+                    isSwapped = true;
+                }
+            }
+            if(!isSwapped){
+                break;
+            }
+        }
+        return nums;
+    }
 }
