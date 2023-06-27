d7594e14271a7dac56a2b61575c06ce183867b8b
Atif Iqbal Butt <96383720+AtifButt123@users.noreply.github.com>
2023-06-27 08:22:06 +0500

    Update InsertionSort.java

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class InsertionSort {
+public class InsertionSort implements ISortingStrategy{
+    @Override
+    public int[] sort(int[]nums) {
+        System.out.println("This is insertion sort");
+        int n = nums.length;
+
+        for(int i = 1; i<n; i++){
+            int j = i-1;
+            int temp = nums[i];
+            while(j>=0 && nums[j]>temp){
+                nums[j+1] = nums[j];
+                j--;
+            }
+            if(j!=i-1)
+                nums[j+1] = temp;
+        }
+        return nums;
+    }
 }
